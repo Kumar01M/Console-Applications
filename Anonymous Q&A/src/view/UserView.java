dbe9242a1dacd40e076dbb40cccb8d7c7e310068
Kumar <104199783+Kumar01M@users.noreply.github.com>
2022-11-14 06:44:12 +0530

    Add files via upload

--- Anonymous Q&A/src/view/UserView.java
+++ Anonymous Q&A/src/view/UserView.java
@@ -2,30 +2,32 @@ package view;
 
 import java.util.Scanner;
 
+import controller.AdminController;
 import controller.UserController;
 import controller.Validate;
 
 public class UserView {
-	
 	public static Scanner in = new Scanner(System.in);
 	private UserController userController;
+	private AdminController adminController;
+	
 	
 	private UserView(){
+		adminController = new AdminController();
 		userController = new UserController();
 	}
 	
 	public void init() {
 		int choice = 0;
-		while(choice!=3) {
-			System.out.println("Welcome to Anonymous Q&A Platform...\nAsk your Questions...");
-			System.out.println("\t[1] Login");
-			System.out.println("\t[2] Signin");
-			System.out.println("\t[3] Exit");
+		System.out.println("   Welcome to Anonymous Q&A Platform...\nAsk your Questions...");
+		while(choice!=4) {
+			System.out.println("\t[1] Login\n\t[2] Signup\n\t[3] ADMIN\n\t[4] Exit");
 			System.out.println("------------------------------------------");
 			choice = Validate.validateInteger();
 			if(choice == 1)
 				userController.logInUser();
-			else if(choice==2) userController.signInUser();
+			else if(choice==2) userController.signUpUser();
+			else if(choice==3) adminController.logIn();
 			else {
 				System.out.println("Thank you for using AnOneMyUs Q&A.");
 			}
